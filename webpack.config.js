70bd8700459968dcff06728864dc067a98449890
colthreepv <mister.gamer@gmail.com>
2016-03-14 04:50:02 +0100

    HOT reload with css and react!

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,24 @@
 const path = require('path');
 
 module.exports = {
-  // context: 'src',
-  entry: path.join(__dirname, 'src', 'main.js'),
-  output: { path: __dirname, filename: 'bundle.js' },
+  entry: [
+    path.join(__dirname, 'src', 'main.js')
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
   module: {
     loaders: [
       {
         test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+        loaders: ['react-hot', 'babel-loader'],
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loaders: ['style-loader', 'css-loader?modules']
       }
     ]
   }
