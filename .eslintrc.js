109a4877329220dcd89313169e6d26160a339a1b
colthreepv <mister.gamer@gmail.com>
2016-03-23 14:06:20 +0100

    eslint / webpack fix / redux-thunk added for asynchronicity

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   rules: {
     'arrow-spacing': 2,
-    'brace-style': [2, '1tbs'],
+    'brace-style': [2, '1tbs', { allowSingleLine: true }],
     'comma-style': [2, 'last'],
     curly: [2, 'multi-line'],
     'default-case': 2,
@@ -16,6 +16,7 @@ module.exports = {
     'guard-for-in': 0,
     indent: [2, 2],
     'new-cap': 0,
+    'no-console': 1,
     'no-delete-var': 2,
     'no-undef': 2,
     'no-floating-decimal': 2,
@@ -23,7 +24,7 @@ module.exports = {
     'no-nested-ternary': 2,
     'no-sequences': 0,
     'no-shadow': 0,
-    'no-unused-vars': [1, {vars: 'all', args: 'after-used'}],
+    'no-unused-vars': [1, { vars: 'all', args: 'after-used' }],
     'no-var': 0,
     'object-curly-spacing': [2, 'always'],
     'prefer-const': 2,
@@ -33,9 +34,9 @@ module.exports = {
     'keyword-spacing': 2,
     'space-before-blocks': 2,
     'space-before-function-paren': 2,
-    'spaced-comment': [2, 'always', { exceptions: ['-']}],
+    'spaced-comment': [2, 'always', { exceptions: ['-'] }],
     strict: 0,
-    'valid-jsdoc': [0, { prefer: { return: 'returns'}}]
+    'valid-jsdoc': [0, { prefer: { return: 'returns' } }]
   },
   root: true
 };
