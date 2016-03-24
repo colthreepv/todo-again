e7a45dc6c41b7e575516471442cc4e935b3a1576
colthreepv <mister.gamer@gmail.com>
2016-03-24 17:00:54 +0100

    better int->base62 and safe-base64 modules

--- src/id-generator.js
+++ src/id-generator.js
@@ -1,17 +1,7 @@
-const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+import { base62 } from 'base62';
+
 const MIN_VALUE = Math.pow(62, 7);
 const MAX_VALUE = Math.pow(62, 8);
-
-function base62 (integer) {
-  if (integer === 0) return '0';
-  let s = '';
-  let decrementing = integer;
-  while (decrementing > 0) {
-    s = ALPHABET[decrementing % 62] + s;
-    decrementing = Math.floor(decrementing/62);
-  }
-  return s;
-}
 
 export function idGenerator () {
   const numericVal = Math.floor(Math.random() * (MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE);
