9dfb593b3fd9b9fe168e5509a9481c04e3814cee
colthreepv <mister.gamer@gmail.com>
2017-02-02 03:17:21 +0100

    behavioursubject service prepared

--- src/app/list.service.ts
+++ src/app/list.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-import { TodoList } from './todo';
+import { Todo, TodoElement, TodoList } from './todo';
+import { BehaviorSubject } from 'rxjs/behaviorsubject'
+import { Observable } from 'rxjs/observable';
 
 @Injectable()
 export class ListService {
+  private source: BehaviorSubject<TodoList> = new BehaviorSubject([]);
+  public todos: Observable<TodoList> = this.source.asObservable();
 
   constructor() { }
 
+  // addTodo()
 }
